57074ec9bae83b35f661e06eeb8df250fa01d03d
Albert Havnegjerde <albert.havnegjerde@kdef.cloud>
2019-11-12 09:43:08 +0100

    Change includes

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-#include <Service.grpc.pb.h>
+#include <gen/Service.grpc.pb.h>
 #include <mylibrary/my_class.h>
 #include <boost/regex.hpp>
 #include <grpcpp/grpcpp.h>
 int main()
 {
+  GOOGLE_PROTOBUF_VERIFY_VERSION;
   MyLibrary::MyClass myClass;
   std::cout << "Sum: 2+3=" << myClass.sum(2,3) << std::endl;
   grpc::Status ok;
