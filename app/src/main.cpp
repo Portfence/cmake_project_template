792a654e603a99776066f38a4fe61c7d8776a447
Albert Havnegjerde <albert.havnegjerde@kdef.cloud>
2019-11-18 10:58:26 +0100

    App using mylibrary

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -2,13 +2,24 @@
 #include <gen/Service.grpc.pb.h>
 #include <mylibrary/my_class.h>
 #include <boost/regex.hpp>
+#include <boost/log/trivial.hpp>
 #include <grpcpp/grpcpp.h>
+#include <optional>
+
+#include <experimental/filesystem>
+
+using std::experimental::filesystem::directory_iterator;
+
 int main()
 {
   GOOGLE_PROTOBUF_VERIFY_VERSION;
   MyLibrary::MyClass myClass;
-  std::cout << "Sum: 2+3=" << myClass.sum(2,3) << std::endl;
   grpc::Status ok;
-  myClass.runServer("0.0.0.0:50051");
+  std::cout << "Sum: 2+3=" << myClass.sum(2,3) << std::endl;
+  std::vector<bool> v(10);
+  for (const auto&& e : v)
+  {
+    std::cout << e <<std::endl;
+  }
   return 0;
 }
